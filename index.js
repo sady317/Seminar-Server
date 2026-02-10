b5ce1522c888f7ae4bd9f8bce1d1eb9a827aaaf6
Sady317 <sadychowdhury92@gmail.com>
2026-02-10 21:15:26 +0600

    seminar-server-site

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config()
+const app = express();
+app.use(cors());
+app.use(express.json())
+const port = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//MONGO DB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djciamc.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   const seminarCollection=client.db("SeminarUserDB").collection("Users")
+    await client.connect();
+
+    
+app.post("/seminar",async (req, res) => {
+  const data = req?.body;
+  const result=await seminarCollection.insertOne(data)
+   res.json({
+        success:true,
+        msg:"Successfully Submited",
+        result:result
+      })});
+
+      app.get("/seminar",async(req,res)=>{
+        const result=await seminarCollection.find().toArray()
+        res.send(result)
+      })
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  
+  finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
