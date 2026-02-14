a54099fe3c1f56e3990f1fb3ba3f5e9cc914c7f9
Sady317 <sadychowdhury92@gmail.com>
2026-02-14 20:26:19 +0600

    seminar-server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 const app = express();
 app.use(cors());
-app.use(express.json())
-const port = process.env.PORT || 5000
+app.use(express.json());
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 //MONGO DB
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djciamc.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -20,41 +20,60 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-   const seminarCollection=client.db("SeminarUserDB").collection("Users")
+    const seminarCollection = client.db("SeminarUserDB").collection("Users");
     await client.connect();
 
-    
-app.post("/seminar",async (req, res) => {
-  const data = req?.body;
-  const result=await seminarCollection.insertOne(data)
-   res.json({
-        success:true,
-        msg:"Successfully Submited",
-        result:result
-      })});
+    // CREATE
+    app.post("/seminar", async (req, res) => {
+      const data = req?.body;
+      const result = await seminarCollection.insertOne(data);
+      res.json({
+        success: true,
+        msg: "Successfully Submited",
+        result: result,
+      });
+    });
+    // READ
+    app.get("/seminar", async (req, res) => {
+      const result = await seminarCollection.find().toArray();
+      res.send(result);
+    });
 
-      app.get("/seminar",async(req,res)=>{
-        const result=await seminarCollection.find().toArray()
-        res.send(result)
-      })
-   
+
+    // Delete using body!
+    // app.delete("/delete",async(req,res)=>{
+    //   const id =req.body.id
+    //   const result=await seminarCollection.deleteOne()({_id:new ObjectId(id)})
+    //   res.send(result)
+    // }) 
+
+
+    // Delete using params!
+    app.delete("/delete/:id",async(req,res)=>{
+      const id=req.params.id
+      const result=await seminarCollection.deleteOne({_id:new ObjectId(id)})
+      res.send(result)
+    })
+
+
+ 
+
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  }
-  
-  finally {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
