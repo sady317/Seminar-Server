2455f398a9f258693e95646a7cf414d00fde6efb
Sady317 <sadychowdhury92@gmail.com>
2026-02-15 22:35:42 +0600

    seminar-server

--- index.js
+++ index.js
@@ -38,10 +38,37 @@ async function run() {
         result: result,
       });
     });
+
     // READ
     app.get("/seminar", async (req, res) => {
-      const result = await seminarCollection.find().toArray();
+      const result = await seminarCollection.find().toArray()
       res.send(result);
+    });
+
+    // READ ONE DATA
+    app.get("/seminar/:id",async(req,res)=>{
+      const id=req.params.id
+      console.log(id)
+      const result=await seminarCollection.findOne({_id:new ObjectId(id)})
+      res.send(result)
+    })
+
+
+
+    // Update name using of patch
+    // This is for update
+    app.patch("/seminar/:id", async (req, res) => {
+      const id=req.params.id
+      const filter={_id:new ObjectId(id)}
+      const newName=req.body.name;
+      console.log(newName,'newName')
+      const updateDoc={
+        $set:{
+          name:newName
+        }
+      };
+      const result=await seminarCollection.updateOne(filter,updateDoc)
+      res.send(result)
     });
 
 
