3881368236e4b7dc22bbde83e5385a8c0b590b7e
Sady317 <sadychowdhury92@gmail.com>
2026-04-18 15:09:30 +0600

    Seminar-Server

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
     const seminarCollection = client.db("SeminarUserDB").collection("Users");
     await client.connect();
 
@@ -91,10 +96,7 @@ async function run() {
  
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -107,4 +109,5 @@ app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
 
+module.exports=app
 
